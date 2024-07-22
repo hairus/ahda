3edc0bf17a79aaf0554034a9de2295e030e2a3b1
adhfry <ahda.creator@gmail.com>
2024-07-22 19:18:03 +0700

    create layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}', 'node_modules/preline/dist/*.js',],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'custom': '0.5px 0 5px rgba(0, 0, 0, 0.3)', // Define custom shadow here
+      },
+      fontFamily: {
+        'sans': ['Poppins', 'sans-serif'], 
+        'roboto': ['Roboto', 'sans-serif']
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
